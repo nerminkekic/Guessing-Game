71e5efbcfc40a2c2504cbf71487894f5d3ccf4e8
Nermin Kekic <nermink@gmail.com>
2019-11-06 22:28:22 -0600

    added while loop to script

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,19 +1,21 @@
 # Write a programme where the computer randomly generates a number between 0 and 5.
 # The user needs to guess what the number is.
 # If the user guesses wrong, tell them their guess is either too high, or too low.
-
 import random
+
 
 guess = int(input("Guess a number between 0 and 5! "))
 randomNumb = random.randrange(1)
-if guess == randomNumb:
-    print("You guessed " + str(guess))
-    print("Your guess is correct.")
-else:
-    print("You guessed " + str(guess))
-    if guess < randomNumb:
-        print("Your guess was low.")
-    else:
-        print("Your guess was high.")
-        
-print("Correct answer was: " + str(randomNumb))
+while guess != randomNumb:
+    
+    if guess == randomNumb:
+        print("You guessed " + str(guess))
+        print("Your guess is correct.")
+        break
+    elif guess < randomNumb:
+        print("You guessed low!")
+        guess = int(input("Guess a number between 0 and 5! "))
+        continue
+    elif guess > randomNumb:
+        print("Your guessed high!")
+        guess = int(input("Guess a number between 0 and 5! "))
