c01489f3552ab4f05bf137671d71ed6c61267eae
Nermin Kekic <nermink@gmail.com>
2019-11-07 21:42:27 -0600

    Added comments

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,21 +1,20 @@
-# Write a programme where the computer randomly generates a number between 0 and 5.
+# Write a programme where the computer randomly generates a number between 0 and 20.
 # The user needs to guess what the number is.
 # If the user guesses wrong, tell them their guess is either too high, or too low.
 import random
 
-
-guess = int(input("Guess a number between 0 and 5! "))
-randomNumb = random.randrange(1)
+# Take input from user
+guess = int(input("Guess a number between 0 and 20! "))
+# Generate random number
+randomNumb = random.randrange(20)
+# Allow user to guess again if first guess is not correct
+# Also check for conditions and let user know if they guessed high or low
 while guess != randomNumb:
-    
-    if guess == randomNumb:
-        print("You guessed " + str(guess))
-        print("Your guess is correct.")
-        break
-    elif guess < randomNumb:
+    if guess < randomNumb:
         print("You guessed low!")
-        guess = int(input("Guess a number between 0 and 5! "))
-        continue
+        guess = int(input("Try another guess. "))
     elif guess > randomNumb:
         print("Your guessed high!")
-        guess = int(input("Guess a number between 0 and 5! "))
+        guess = int(input("Try another guess. "))
+print("You have guessed correct. YOU WIN!")
+
